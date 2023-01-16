495130f5ae7ba1d18e30d778631408d48a93c039
athif <athifirshad@hotmail.com>
2023-01-16 21:44:27 +0530

    Refactored and add new clipboard functionality

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
 document.getElementById("genbtn").addEventListener("click", generate);
 
-
 function generate(){
-    const firstPass = []
-    const secondPass = []
-        
-    while (firstPass.length < 15){
-    let i = (Math.floor((Math.random()) * characters.length))
-    let j = (Math.floor((Math.random()) * characters.length))
-    firstPass.push(characters[i])
-    secondPass.push(characters[j])
-    }
+    let firstPass = []
+    let secondPass = []
 
+    for(let i = 0; i < 15; i++){
+      let random = Math.floor(Math.random() * characters.length);
+      firstPass.push(characters[random]);
+      random = Math.floor(Math.random() * characters.length);
+      secondPass.push(characters[random]);
+    }
     let firstStr = firstPass.join('')
     let secondStr = secondPass.join('')
-    console.log(firstStr)
-    console.log(secondStr)
-    document.getElementById("pass1field").innerText = firstStr
-    document.getElementById("pass2field").innerText = secondStr
+    document.getElementById("pass1field").textContent = firstStr
+    document.getElementById("pass2field").textContent = secondStr
+}
 
-    
-}   
+document.getElementById("copy1").addEventListener("click", function(){
+    copyToClipboard("pass1field","copy1");
+});
 
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+document.getElementById("copy2").addEventListener("click", function(){
+    copyToClipboard("pass2field","copy2");
+});
+
+async function copyToClipboard(id,btn) {
+  let text = document.getElementById(id).innerText;
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log("Password copied to clipboard");
+    document.getElementById(btn).textContent = "Copied!";
+  } catch (err) {
+    console.error("Failed to copy password: ", err);
+  }
+}
